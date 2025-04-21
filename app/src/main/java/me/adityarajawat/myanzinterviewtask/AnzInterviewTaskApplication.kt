16b4ee2eb10a0fc894b67f0f969e6d2d3731ed17
aditya147 <aditya.dijango@gmail.com>
2025-04-21 18:46:58 +0530

    Add application class and internet permission

---
+++ app/src/main/java/me/adityarajawat/myanzinterviewtask/AnzInterviewTaskApplication.kt
@@ -0,0 +1,7 @@
+package me.adityarajawat.myanzinterviewtask
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AnzInterviewTaskApplication : Application()
